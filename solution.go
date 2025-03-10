52f9e4eeb9d365188151d8f8d1bbddef5e9fecf1
gharjen <george.barton@mqube.com>
2025-03-10 08:59:44 +0000

    feat: build out game functions

--- solution.go
+++ solution.go
@@ -1,5 +1,62 @@
 package main
 
-func solution(x bool) bool {
-	return !x
+import "strings"
+
+func main() {}
+
+func simulateGame(h1 []int, h2 []int) {
+	deckSize := len(h1) + len(h2)
+	discard := []int{}
+	isH1Turn := true
+	card := 0
+	for len(h1) < deckSize || len(h2) < deckSize {
+		if isH1Turn {
+			card, h1, discard = placeCard(h1, discard)
+			if card > 0 {
+				playTrick(h1, h2, discard)
+			}
+			isH1Turn = false
+		} else {
+			card, h2, discard = placeCard(h2, discard)
+			if card > 0 {
+				playTrick(h2, h1, discard)
+			}
+			isH1Turn = true
+		}
+	}
+}
+
+func playTrick(initiator []int, player []int, discard []int) ([]int, []int, []int) {
+	turns := discard[len(discard)-1]
+	for i := 0; i < turns; i++ {
+
+	}
+}
+
+// Hand[0] = top
+// Discard[0] = bottom
+func placeCard(hand []int, discard []int) (int, []int, []int) {
+	card, hand := hand[0], hand[1:]
+	discard = append(discard, card)
+	return card, hand, discard
+}
+
+func convertStringIntoGameArray(x string) []int {
+	cards := strings.Split(x, "")
+	result := make([]int, len(cards))
+	for i, card := range cards {
+		switch card {
+		case "A":
+			result[i] = 4
+		case "K":
+			result[i] = 3
+		case "Q":
+			result[i] = 2
+		case "J":
+			result[i] = 1
+		default:
+			result[i] = 0
+		}
+	}
+	return result
 }
--- solution_test.go
+++ solution_test.go
@@ -8,24 +8,63 @@ import (
 func Test(t *testing.T) {
 	testCases := []struct {
 		name string
-		data bool
-		exp  bool
+		data string
+		exp  []int
 	}{
 		{
-			name: "returns true",
-			data: false,
-			exp:  true,
-		},
-		{
-			name: "returns false",
-			data: true,
-			exp:  false,
+			name: "values map correctly",
+			data: "AKQJ-",
+			exp:  []int{4, 3, 2, 1, 0},
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			audit := solution(tc.data)
+			audit := convertStringIntoGameArray(tc.data)
 			assert.Equal(t, tc.exp, audit)
+		})
+	}
+}
+
+func TestPlaceCard(t *testing.T) {
+	testCases := []struct {
+		name       string
+		hand       []int
+		discard    []int
+		handExp    []int
+		discardExp []int
+		cardExp    int
+	}{
+		{
+			name:       "Add card to discard",
+			hand:       []int{0},
+			discard:    []int{},
+			discardExp: []int{0},
+			handExp:    []int{},
+			cardExp:    0,
+		},
+		{
+			name:       "Check correct order",
+			hand:       []int{3, 2, 1, 0},
+			discard:    []int{},
+			discardExp: []int{3},
+			handExp:    []int{2, 1, 0},
+			cardExp:    3,
+		},
+		{
+			name:       "Check correct order",
+			hand:       []int{3, 2, 1, 0},
+			discard:    []int{0, 0, 0},
+			discardExp: []int{0, 0, 0, 3},
+			handExp:    []int{2, 1, 0},
+			cardExp:    3,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cardA, handA, discardA := placeCard(tc.hand, tc.discard)
+			assert.Equal(t, tc.cardExp, cardA)
+			assert.Equal(t, tc.discardExp, discardA)
+			assert.Equal(t, tc.handExp, handA)
 		})
 	}
 }
