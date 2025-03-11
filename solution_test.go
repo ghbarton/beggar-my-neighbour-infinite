fcec90cd232847553a2c43c15f4f9e04adb18013
gharjen <george.barton@mqube.com>
2025-03-11 22:25:42 +0000

    fix: add error handling

--- solution_test.go
+++ solution_test.go
@@ -5,6 +5,28 @@ import (
 	"testing"
 )
 
+func TestSimulateGame(t *testing.T) {
+	testCases := []struct {
+		name  string
+		hand1 string
+		hand2 string
+		exp   int
+	}{
+		{
+			name:  "game 1",
+			hand1: "Q--J----K--K-J---Q---A---A",
+			hand2: "---K-K--JA-QA--J-----Q----",
+			exp:   5104,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			audit := simulateGame(convertStringIntoGameArray(tc.hand1), convertStringIntoGameArray(tc.hand2))
+			assert.Equal(t, tc.exp, audit)
+		})
+	}
+}
+
 func TestValueMapping(t *testing.T) {
 	testCases := []struct {
 		name string
@@ -66,39 +88,43 @@ func TestGiveWinnerCards(t *testing.T) {
 
 func TestPlayTrick(t *testing.T) {
 	testCases := []struct {
-		name    string
-		init    []int
-		player  []int
-		discard []int
-		expInit []int
-		expPlay []int
-		expDisc []int
+		name     string
+		init     []int
+		player   []int
+		discard  []int
+		expInit  []int
+		expPlay  []int
+		expDisc  []int
+		errorExp error
 	}{
 		{
-			name:    "Play trick",
-			init:    []int{},
-			player:  []int{0},
-			discard: []int{1},
-			expInit: []int{1, 0},
-			expPlay: []int{},
-			expDisc: []int{},
+			name:     "Play trick",
+			init:     []int{},
+			player:   []int{0},
+			discard:  []int{1},
+			expInit:  []int{1, 0},
+			expPlay:  []int{},
+			expDisc:  []int{},
+			errorExp: nil,
 		},
 		{
-			name:    "Initiator looses",
-			init:    []int{0, 0, 0, 0},
-			player:  []int{1},
-			discard: []int{1},
-			expInit: []int{0, 0, 0},
-			expPlay: []int{1, 1, 0},
-			expDisc: []int{},
+			name:     "Initiator looses",
+			init:     []int{0, 0, 0, 0},
+			player:   []int{1},
+			discard:  []int{1},
+			expInit:  []int{0, 0, 0},
+			expPlay:  []int{1, 1, 0},
+			expDisc:  []int{},
+			errorExp: nil,
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			initA, playerA, discardA := playTrick(tc.init, tc.player, tc.discard)
+			initA, playerA, discardA, err := playTrick(tc.init, tc.player, tc.discard)
 			assert.Equal(t, tc.expInit, initA)
 			assert.Equal(t, tc.expPlay, playerA)
 			assert.Equal(t, tc.expDisc, discardA)
+			assert.Equal(t, tc.errorExp, err)
 		})
 	}
 }
@@ -111,6 +137,7 @@ func TestPlaceCard(t *testing.T) {
 		handExp    []int
 		discardExp []int
 		cardExp    int
+		errorExp   error
 	}{
 		{
 			name:       "Add card to discard",
@@ -119,6 +146,7 @@ func TestPlaceCard(t *testing.T) {
 			discardExp: []int{0},
 			handExp:    []int{},
 			cardExp:    0,
+			errorExp:   nil,
 		},
 		{
 			name:       "Check correct order",
@@ -127,6 +155,7 @@ func TestPlaceCard(t *testing.T) {
 			discardExp: []int{3},
 			handExp:    []int{2, 1, 0},
 			cardExp:    3,
+			errorExp:   nil,
 		},
 		{
 			name:       "Check correct order",
@@ -135,14 +164,16 @@ func TestPlaceCard(t *testing.T) {
 			discardExp: []int{0, 0, 0, 3},
 			handExp:    []int{2, 1, 0},
 			cardExp:    3,
+			errorExp:   nil,
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			handA, discardA := placeCard(tc.hand, tc.discard)
+			handA, discardA, err := placeCard(tc.hand, tc.discard)
 			assert.Equal(t, tc.cardExp, discardA[len(discardA)-1])
 			assert.Equal(t, tc.discardExp, discardA)
 			assert.Equal(t, tc.handExp, handA)
+			assert.Equal(t, tc.errorExp, err)
 		})
 	}
 }
